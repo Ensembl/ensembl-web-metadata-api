b03d95deff5c8244260835a0e7a08442e5d47574
Kamal Dodiya <kamal@ebi.ac.uk>
2023-06-19 21:44:16 +0100

    Ensure the grpc client working in Docker

--- app/api/resources/grpc_client.py
+++ app/api/resources/grpc_client.py
@@ -14,13 +14,6 @@
 import grpc
 from ensembl.production.metadata import ensembl_metadata_pb2
 from ensembl.production.metadata import ensembl_metadata_pb2_grpc
- 
- 
-# Create Channel
-channel = grpc.insecure_channel('{}:{}'.format("localhost", 50051), options=(('grpc.enable_http_proxy', 0),))
- 
-# Create Stub 
-stub = ensembl_metadata_pb2_grpc.EnsemblMetadataStub(channel)
 
 class GRPCClient:
 	def __init__(self, host:str, port:int):
@@ -30,7 +23,7 @@ class GRPCClient:
 		self.channel = grpc.insecure_channel('{}:{}'.format(self.host, self.port), options=(('grpc.enable_http_proxy', 0),))
  
 		# Create Stub 
-		self.stub = ensembl_metadata_pb2_grpc.EnsemblMetadataStub(channel)
+		self.stub = ensembl_metadata_pb2_grpc.EnsemblMetadataStub(self.channel)
 
 	def get_statistics(genome_id:str):
 		return {}
