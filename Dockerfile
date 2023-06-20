ae6f002ee22f3cddf0fffd946f6541c1f7ad0026
Kamal Dodiya <40833799+kamaldodiya@users.noreply.github.com>
2023-06-20 11:39:28 +0100

    GRPC Service Integration (#3)

--- Dockerfile
+++ Dockerfile
@@ -18,17 +18,14 @@ FROM tiangolo/uvicorn-gunicorn:python3.8
 ENV PORT 8083
 EXPOSE 8083
 RUN apt-get update && \
-    apt-get install -y --no-install-recommends netcat && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-# RUN mkdir app
-
-# WORKDIR /app
 
 COPY ./app/ /app/
 
+WORKDIR /app
+
 COPY poetry.lock pyproject.toml ./
-RUN pip install poetry==1.0.* && \
+RUN pip install poetry && \
     poetry config virtualenvs.create false && \
     poetry install --no-dev
 
---
+++ app/api/resources/grpc_client.py
@@ -0,0 +1,39 @@
+"""
+.. See the NOTICE file distributed with this work for additional information
+   regarding copyright ownership.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+import grpc
+from ensembl.production.metadata import ensembl_metadata_pb2
+from ensembl.production.metadata import ensembl_metadata_pb2_grpc
+
+class GRPCClient:
+	def __init__(self, host:str, port:int):
+		self.host = host
+		self.port = port
+		# Create Channel
+		self.channel = grpc.insecure_channel('{}:{}'.format(self.host, self.port), options=(('grpc.enable_http_proxy', 0),))
+ 
+		# Create Stub 
+		self.stub = ensembl_metadata_pb2_grpc.EnsemblMetadataStub(self.channel)
+
+	def get_statistics(genome_id:str):
+		return {}
+
+	def get_genome(self, genome_uuid:str):
+		# Create request
+		request = ensembl_metadata_pb2.GenomeUUIDRequest(genome_uuid=genome_uuid, release_version=None)
+ 
+		# Get response
+		response = self.stub.GetGenomeByUUID(request)
+
+		return response
+
