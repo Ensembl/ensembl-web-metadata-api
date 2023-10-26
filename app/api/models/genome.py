20f27f1f2f3cf98c0d1554eb9566081ef2ec9f17
Mehrnaz Charkhchi <mehrnaz.charkhchi@gmail.com>
2023-10-26 11:30:59 +0100

    Update to AliasPath.

--- app/api/models/genome.py
+++ app/api/models/genome.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Optional
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, validator, AliasChoices, AliasPath
 
 from core.logging import InterceptHandler
 from core.config import ENA_ASSEMBLY_URL
@@ -38,51 +38,40 @@ class AnnotationProvider(BaseModel):
 
 class GenomeDetails(BaseModel):
     genome_id: str = Field(alias='genomeUuid')
-    genome_tag: Optional[str] = Field(alias='assembly', default=None)
-    taxonomy_id: str
-    species_taxonomy_id: str
-    common_name: str = None
-    scientific_name: str
+    genome_tag: Optional[str] = Field(
+        alias=AliasChoices(AliasPath('assembly', 'urlName'), AliasPath('assembly', 'tolId')), default=None)
+    taxonomy_id: str = Field(alias=AliasPath('organism', 'taxonomyId'))
+    species_taxonomy_id: str = Field(alias=AliasPath('organism', 'speciesTaxonomyId'))
+    common_name: str  = Field(alias=AliasPath('organism', 'commonName'), default=None)
+    scientific_name: str = Field(alias=AliasPath('organism', 'scientificName'))
     type: Optional[Type] = None
-    is_reference: bool = Field(alias='isReference', default=False)
+    is_reference: bool = Field(alias=AliasPath('assembly', 'isReference'),default=False)
     assembly: AssemblyInGenome = None
     assembly_provider: AssemblyProvider = None
-    assembly_level: str
-    assembly_date: str
+    assembly_level: str = Field(alias=AliasPath('attributesInfo', 'assemblyLevel'))
+    assembly_date: str = Field(alias=AliasPath('attributesInfo', 'assemblyDate'))
     annotation_provider: AnnotationProvider = None
     annotation_method: str = Field(alias='genebuildMethod', default=None)  # REMOVE DEFAULT
     annotation_version: str = Field(alias='genebuildVersion', default=None)
     annotation_date: str = Field(alias='created')
     number_of_genomes_in_group: int = Field(alias='relatedAssembliesCount', default=1)
 
+    @validator("taxonomy_id", "species_taxonomy_id", pre=True)
+    def concert_int_to_str(cls, value):
+        return str(value)
+
     @validator("annotation_date", pre=True)
     def parse_date(cls, value):
         year, month, _ = value.split("-")
         return year + '-' + month
 
-    @validator("genome_tag", pre=True)
-    def fetch_genome_tag_value(cls, value):
-        if value and type(value) is dict:
-            if value.get("urlName", None):
-                return value.get("urlName", None)
-            return value.get("tolId", None)
-        return value
-
     def __init__(self, **data):
-        data["taxonomy_id"] = str(data.get("organism", {}).get("taxonomyId"))
-        data["species_taxonomy_id"] = str(data.get("organism", {}).get("speciesTaxonomyId", None))
-        data["common_name"] = data.get("organism", {}).get("commonName", None)
-        data["scientific_name"] = data.get("organism", {}).get("scientificName", None)
-        data["isReference"] = data.get("assembly", {}).get('isReference', False)
-        data["assembly_level"] = data.get("attributesInfo", {}).get('assemblyLevel')
-        data["assembly_date"] = data.get("attributesInfo", {}).get('assemblyDate', None)
-
         if data.get("organism", {}).get('strainType', None):
             data["type"] = {
                 "kind": data.get("organism", {}).get("strainType", None),
                 "value": data.get("organism", {}).get("strain", None),
             }
-        if data.get("attributesInfo", {}).get('AssemblyProviderName', None): #TODO: Update attributesInfo
+        if data.get("attributesInfo", {}).get('AssemblyProviderName', None):  # TODO: Update attributesInfo
             data["assembly_provider"] = {
                 "name": data.get("attributesInfo", {}).get("AssemblyProviderName", None),
                 "url": data.get("attributesInfo", {}).get("AssemblyProviderUrl", None),
