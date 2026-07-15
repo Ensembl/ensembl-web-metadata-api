206452654976acff5ee3999ef26fc0a6b41b5739
Jon Keatley <jon@ebi.ac.uk>
2026-07-15 12:21:25 +0100

    Added statement to clear old url_names

--- sql/011_overwrite_url_name.sql
+++ sql/011_overwrite_url_name.sql
@@ -1,5 +1,10 @@
 # Overwrite url_name for integrated genomes setting it to assembly.accession. 
 # WARNING! This can create duplicates that need to be addressed by the accession-map script
+
+# Clear existing url_names
+UPDATE genome SET url_name = '';
+
+# Set url_name to accession for current integrated genomes
 WITH accession_map as (SELECT g.genome_id, a.accession
 FROM genome g
 JOIN assembly a ON g.assembly_id = a.assembly_id
@@ -8,4 +13,4 @@ JOIN ensembl_release er ON gr.release_id = er.release_id
 WHERE er.release_type = 'integrated' and er.is_current = 1 and g.suppressed = 0)
 UPDATE genome SET url_name = accession_map.accession
 FROM accession_map
-where genome.genome_id = accession_map.genome_id
+where genome.genome_id = accession_map.genome_id;
